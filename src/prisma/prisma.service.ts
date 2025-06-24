29c24e047bb07ae53fb8c69bf63a6d268fd9f5d4
Adilson Futa <adilsonmariafuta@hotmail.com>
2025-06-24 20:46:08 +0200

    ajustando prismaservice

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,17 +1,23 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { INestApplication, Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from 'generated/prisma';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit{
+export class PrismaService extends PrismaClient implements OnModuleInit,OnModuleDestroy{
 
    async onModuleInit() {
       await this.$connect();
    }
 
-   async enableHook(app: INestApplication){
-    process.on('beforeExit', async () => {
-        await app.close();
+   async onModuleDestroy() {
+      await this.$disconnect();
+   }
+}
+
+   /**
+    * @param app 
+    * async enableHook(app: INestApplication){
+         process.on('beforeExit', async () => {
+         await app.close();
     })
    }
-
-}
+    */
