1bbe95dda931d01eec9dd7c252dbba745e45912e
Adilson Futa <adilsonmariafuta@hotmail.com>
2025-06-27 00:36:32 +0200

    criando base da api swagguer

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AppService } from './app.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function main() {
+  // # config app inicial
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+
+// # config app swagger docs api
+
+const config = new DocumentBuilder()
+.setTitle('Api de Lugares')
+.setDescription('Api com documentacao estilo open api')
+.setVersion('1.0')
+.build();
+
+const document = SwaggerModule.createDocument(app,config);
+SwaggerModule.setup('api',app,document);
+
   await app.listen(process.env.PORT ?? 3000);
-  console.log(new AppService().smsLoading);
+  console.log(`${new AppService().smsLoading} Port ${process.env.PORT}`);
 }
 main();
