87977dbe5993a48f0d7df97b67cac4aeada6af4e
Adilson Futa <adilsonmariafuta@hotmail.com>
2025-07-01 21:55:09 +0200

    feito toda documentacao

--- src/lugar/dto/update-lugar.dto.ts
+++ src/lugar/dto/update-lugar.dto.ts
@@ -1,6 +1,15 @@
-import { PartialType } from '@nestjs/mapped-types';
+//import { PartialType } from '@nestjs/mapped-types';
 import { CreateLugarDto } from './create-lugar.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/swagger';
 
 export class UpdateLugarDto extends PartialType(CreateLugarDto) {
+    
+    @ApiProperty({
+    description: 'Status do lugar',
+    example: 'ativo',
+    required: false,
+  })
+  status?: string;
 
 }
